63d51d6877e3a545e588874782d6cb19f7da34f3
facccge <fcfc5702@qq.com>
2019-02-21 16:45:17 +0800

    add test case of loadAllItems()

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,8 +1,31 @@
-describe('Take out food', function () {
+describe('Load all items', function () {
+  it('should load all items when use loadAllItems() function', function() {
+    let summary = loadAllItems();
+    let expected = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      price: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      price: 6.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      price: 8.00
+    }, {
+      id: 'ITEM0030',
+      name: '冰锋',
+      price: 2.00
+    }];
+    expect(summary).toEqual(expected)
+  });
+});
 
+describe('Take out food', function () {
   it('should generate best charge when best is 指定菜品半价', function() {
     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
+    let summary = String(bestCharge(inputs)).trim();
     let expected = `
 ============= 订餐明细 =============
 黄焖鸡 x 1 = 18元
@@ -13,13 +36,13 @@ describe('Take out food', function () {
 指定菜品半价(黄焖鸡，凉皮)，省13元
 -----------------------------------
 总计：25元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
 
   it('should generate best charge when best is 满30减6元', function() {
     let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
-    let summary = bestCharge(inputs).trim();
+    let summary = String(bestCharge(inputs)).trim();
     let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
@@ -29,19 +52,19 @@ describe('Take out food', function () {
 满30减6元，省6元
 -----------------------------------
 总计：26元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
 
   it('should generate best charge when no promotion can be used', function() {
     let inputs = ["ITEM0013 x 4"];
-    let summary = bestCharge(inputs).trim();
+    let summary = String(bestCharge(inputs)).trim();
     let expected = `
 ============= 订餐明细 =============
 肉夹馍 x 4 = 24元
 -----------------------------------
 总计：24元
-===================================`.trim()
+===================================`.trim();
     expect(summary).toEqual(expected)
   });
 
